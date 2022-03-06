deb0af425607471f1138fe74e65fa84c392d35d9
Wayne Robinson <l00162024@student.lyit.ie>
2022-03-06 16:02:35 +0000

    Finished, Working.

--- Question2.py
+++ Question2.py
@@ -2,27 +2,42 @@
 # Author: Wayne Robinson
 # Number: 21205974
 # Python Script Assignment1
-# Script: Question1.py
+# Script: Question2.py
 
+# List Variable declaration
 sentence = ''
 third_letter_sentence =''
 vowel_sentence = ''
 
-#def user_input(sentence):
-sentence = input("Please enter sentence: ")
-print("The sentence enter by the user is: " + sentence)
-#def third_letter(sentence):
+# Display Function
+def display_anwers():
+    print("\n===================================================")
 
+#=================== Question 2 Part A =========================
+# Function call
+display_anwers()
+
+# Prompt user and take in sentence from user
+sentence = input("Please enter sentence: ")
+print("\nThe sentence enter by the user is: " + sentence)
+
+#=================== Question 2 Part B =========================
+display_anwers()
+
+# String split, starting at postion 0, finish at the length of the sentence, with a step size of 3.
 third_letter_sentence = sentence[0:len(sentence):3]
 print("The new sentence with every third letter print out is: ", third_letter_sentence)
-    #return new_sentence
 
+#=================== Question 2 Part C =========================
+display_anwers()
+
+# Iterate through sentence list.
 for letter in sentence:
 
-    if letter in 'aeiou':
+    # If statement to detect any vowels present in string, if so add to new string.
+    if letter in 'aeiou' or letter in 'AEIOU':
         vowel_sentence = vowel_sentence + letter
+    else:
+        pass
 
 print("The new sentence with each vowel printed is as follows: ", vowel_sentence)
-
-#user_input(sentence)
-#print(third_letter(sentence))
